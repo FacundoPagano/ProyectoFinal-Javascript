2f8e0998411cf1ee2c925ec35b3fe8245b9cc651
Facundo Martinez Pagano <facupaganoo@gmail.com>
2022-09-14 22:03:41 -0300

    agregamos carrito

--- js/main.js
+++ js/main.js
@@ -1,89 +1,119 @@
-// Mensaje de bienvenida
-let nombre= prompt("Ingrese su nombre")
-alert ("Bienvenido " + nombre + ", a la tienda online de ropa Nikers, a continuacion eliga la prenda que desea comprar. Cualquier compra mayor a $20.000 tendra un descuento del 10%")
-
 //Variables
-let prenda = 0;
-let totalFinal = 0;
-const carrito = []
-
+const bienvenida= document.getElementById("bienvenida");
+const titulo= document.getElementById("titulo")
+const carrito = document.querySelector('#carrito');
+const listaProductos = document.querySelector('#catalogo');
+const contenedorCarrito = document.querySelector('#carrito-obj tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar'); 
+let articulosCarrito = [];
 //Funciones
-function elegirPrenda() {
-    prenda = parseInt(prompt("Ingrese el numero de la prenda que desea adquirir: \n 1-Remera $2.500\n 2-Campera $5.000\n 3-Buzo $6.000\n 4-Pantalon $4.000\n 5-Zapatillas $8.000 \n 6-Finalizar compra"));
+
+//Procedimiento
+bienvenida.innerText= "Bienvenido a la tienda online de ropa Nikers.";
+titulo.innerText= "NIKERS"
+
+// Listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+     listaProductos.addEventListener('click', agregarProducto);
+
+     carrito.addEventListener('click', eliminarProducto);
+
+     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
 }
 
-function productos(categoria,precio,id){
-    this.categoria=categoria;
-    this.precio=precio;
-    this.id=id;
+
+
+
+// Funciones
+function agregarProducto(e) {
+     e.preventDefault();
+
+     if(e.target.classList.contains('agregar-carrito')) {
+          const producto = e.target.parentElement.parentElement;
+
+          leerDatosProducto(producto);
+     }
 }
 
-const remera= new productos("Remera",2500,1);
-const campera= new productos("Campera",5000,2);
-const buzo= new productos("Buzo",6000,3);
-const pantalon= new productos("Pantalon",4000,4);
-const zapatillas= new productos("Zapatillas",8000,5);
+
+function leerDatosProducto(producto) {
+     const infoProducto = {
+          imagen: producto.querySelector('img').src,
+          titulo: producto.querySelector('h3').textContent,
+          precio: producto.querySelector('.precio').textContent,
+          material: producto.querySelector('.material').textContent,
+          id: producto.querySelector('button').getAttribute('data-id'), 
+          cantidad: 1
+     }
 
 
-function agregarCarrito(productos) {
-    carrito.push(productos)
+     if( articulosCarrito.some( producto => producto.id === infoProducto.id ) ) { 
+          const productos = articulosCarrito.map( producto => {
+               if( producto.id === infoProducto.id ) {
+                    producto.cantidad++;
+                     return producto;
+                } else {
+                     return producto;
+             }
+          })
+          articulosCarrito = [...productos];
+     }  else {
+          articulosCarrito = [...articulosCarrito, infoProducto];
+     }
+
+     console.log(articulosCarrito)
+
+     
+     carritoHTML();
 }
 
-function sumaTotal() {
-    switch (prenda) {
-        case 1:
-            alert ("Usted selecciono Remera, se agregara el costo de $2500 a su carrito");
-            totalFinal += remera.precio;
-            agregarCarrito(remera);
-            break;
-        case 2:
-            alert ("Usted selecciono Campera, se agregara el costo de $5000 a su carrito");
-            totalFinal += campera.precio;
-            agregarCarrito(campera);
-            break;
-        case 3:
-            alert ("Usted selecciono Buzo, se agregara el costo de $6000 a su carrito");
-            totalFinal += buzo.precio;
-            agregarCarrito(buzo);
-            break;
-        case 4:
-            alert ("Usted selecciono Pantalon, se agregara el costo de $4000 a su carrito");
-            totalFinal += pantalon.precio;
-            agregarCarrito(pantalon);
-            break;
-        case 5:
-            alert ("Usted selecciono Zapatillas, se agregara el costo de $8000 a su carrito");
-            totalFinal += zapatillas.precio;
-            agregarCarrito(zapatillas);
-            break;
-        default:
-            alert ("Opcion no valida, porfavor ingrese un numero del 1-5 para poder seguir con su compra su compra");
-            break;
-    }
-    alert("Su total hasta el momento es de: $" + totalFinal);
-    return totalFinal;
+
+function eliminarProducto(e) {
+     e.preventDefault();
+     if(e.target.classList.contains('borrar-producto') ) {
+          const productoId = e.target.getAttribute('data-id')
+          
+          articulosCarrito = articulosCarrito.filter(producto=> producto.id !== productoId);
+
+          carritoHTML();
+     }
 }
 
-function sumarProductos() {
-    const total = carrito.reduce(
-      (acc, el) => (acc += el.precio),0);
-    const descuento = total - total * 0.15;
-    if (total > 20000) {
-      alert("Su compra es mayor a $20.000 por lo tanto obtiene un descuento del 15%. Su total es: " + descuento);
-      console.log("Su compra es mayor a $20.000 por lo tanto obtiene un descuento del 15%. Su total es: " + descuento);
-    } else {
-      alert("Su total es de: " + resultado);
-      console.log("Su total es de: " + resultado);
-    }
-  }
 
-elegirPrenda();
-while(prenda != 6){
-   sumaTotal();
-   elegirPrenda(); 
+
+function carritoHTML() {
+
+     vaciarCarrito();
+
+     articulosCarrito.forEach(producto => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+               <td>  
+                    <img src="${producto.imagen}" width=100>
+               </td>
+               <td>${producto.titulo}</td>
+               <td>${producto.precio}</td>
+               <td>${producto.cantidad} </td>
+               <td>
+                    <button class="borrar-producto" data-id="${producto.id}">X</button>
+               </td>
+          `;
+          contenedorCarrito.appendChild(row);
+     });
+
 }
 
-sumarProductos()
-console.log(carrito);
 
+function vaciarCarrito() {
+
+
+
+
+     while(contenedorCarrito.firstChild) {
+          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+      }
+}
 
